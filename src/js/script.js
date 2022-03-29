e21f470b1185201144bb3f758ee360833f0e3537
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-03-29 16:28:46 +0300

    Добавление страницы Contact

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,19 @@
 "use strict";
 
-const burger = document.querySelector('.header__burger');
-const menu = document.querySelector('.header__menu');
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__menu");
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('header__burger--active');
-    menu.classList.toggle('header__menu--active');
+burger.addEventListener("click", () => {
+    burger.classList.toggle("header__burger--active");
+    menu.classList.toggle("header__menu--active");
+});
+
+const submitButton = document.getElementById("submit_form");
+const form = document.getElementById("email_form");
+
+form.addEventListener("submit", (e) => {
+    setTimeout(function () {
+        submitButton.value = "Отправка...";
+        submitButton.disabled = true;
+    }, 1);
 });
