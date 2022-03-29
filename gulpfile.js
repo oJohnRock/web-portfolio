d3025e3f3d4ede569e42c0785b85298298da584a
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-03-29 11:31:12 +0300

    Обновление настроек Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,88 @@
-const { src, dest, parallel, series, watch } = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify-es').default;
-const sourceMaps = require('gulp-sourcemaps');
-const imagecomp = require('compress-images');
-const rename = require('gulp-rename');
-const del = require('del');
+import gulp from "gulp";
+import browserSync from "browser-sync";
+import sourceMaps from "gulp-sourcemaps";
+import gulpSass from "gulp-sass";
+import sass from "sass";
+import autoprefixer from "gulp-autoprefixer";
+import uglifyes from "gulp-uglify-es";
+import imagemin from "gulp-imagemin";
+import newer from "gulp-newer";
+import rename from "gulp-rename";
+import del from "del";
+
+const { src, dest, watch, series, parallel } = gulp;
+const scss = gulpSass(sass);
+const uglify = uglifyes.default;
 
 function startServer() {
     browserSync.init({
-        server: { baseDir: 'app/' },
+        server: { baseDir: "app/" },
         notify: true,
         online: true,
     });
 
-    watch('app/**/*.html').on('change', browserSync.reload);
-    watch('src/scss/**/*', cssHandler);
-    watch('src/js/**/*', jsHandler);
-    watch('src/fonts/**/*', fontsHandler);
-    watch('src/img/**/*', imgHandler);
+    watch("app/**/*.html").on("change", browserSync.reload);
+    watch("src/scss/**/*", cssHandler);
+    watch("src/fonts/**/*", fontsHandler);
+    watch("src/js/**/*", jsHandler);
+    watch("src/img/**/*", imgHandler);
 }
 
 function cssHandler() {
-    return src('src/scss/style.scss')
+    return src("src/scss/style.scss")
         .pipe(sourceMaps.init())
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourceMaps.write('./'))
-        .pipe(dest('app/css/'))
-        .pipe(browserSync.stream());
-}
-
-function jsHandler() {
-    return src('src/js/script.js')
-        .pipe(uglify())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(dest('app/js/'))
+        .pipe(scss({ outputStyle: "compressed" }))
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 10 versions"],
+                grid: true,
+            })
+        )
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(sourceMaps.write("./"))
+        .pipe(dest("app/css/"))
         .pipe(browserSync.stream());
 }
 
 function fontsHandler() {
-    return src('src/fonts/**/*')
-        .pipe(dest('app/fonts/'));
+    return src("src/fonts/**/*").pipe(dest("app/fonts/"));
+}
+
+function jsHandler() {
+    return src("src/js/script.js")
+        .pipe(uglify())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(dest("app/js/"))
+        .pipe(browserSync.stream());
 }
 
 function imgHandler() {
-    imagecomp(
-        'src/img/**/*',
-        'app/img/',
-        { compress_force: false, statistic: true, autoupdate: true }, false,
-        { jpg: { engine: "mozjpeg", command: ["-quality", "75"] } },
-        { png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
-        { svg: { engine: "svgo", command: "--multipass" } },
-        { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
-        function (err, completed) {
-            if (completed === true) {
-                browserSync.reload();
-            }
-        }
-    );
+    return src("src/img/**/*")
+        .pipe(newer("app/img"))
+        .pipe(imagemin())
+        .pipe(gulp.dest("app/img"));
+}
+
+function imgClean() {
+    return del("app/img/**/*", { force: true });
 }
 
 function buildCopy() {
-    return src('app/**/*', { base: 'app/' })
-        .pipe(dest('build/'));
+    return src("app/**/*", { base: "app/" }).pipe(dest("build/"));
 }
 
 function buildClean() {
-    return del('build/**/*', { force: true });
+    return del("build/**/*", { force: true });
 }
 
-exports.startServer = startServer;
-exports.cssHandler = cssHandler;
-exports.jsHandler = jsHandler;
-exports.fontsHandler = fontsHandler;
-exports.imgHandler = imgHandler;
-exports.buildClean = buildClean;
-exports.buildCopy = buildCopy;
-
-exports.default = series(
-    parallel(
-        cssHandler,
-        jsHandler,
-        fontsHandler),
-    startServer);
-
-exports.build = series(
-    parallel(
-        cssHandler,
-        jsHandler,
-        fontsHandler),
+const start = series(
+    parallel(cssHandler, fontsHandler, jsHandler, imgHandler),
+    startServer
+);
+export const build = series(
+    parallel(cssHandler, fontsHandler, jsHandler, imgHandler),
     buildClean,
-    buildCopy);
+    buildCopy
+);
+
+export default start;
