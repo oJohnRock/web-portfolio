8fb50439571c3d360706d7a1d51a73ecc2800843
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-04-01 14:28:24 +0300

    Сначала

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,9 @@
 "use strict";
 
+// Бургер-меню
 const burger = document.querySelector(".header__burger");
 const menu = document.querySelector(".header__menu");
-
 burger.addEventListener("click", () => {
     burger.classList.toggle("header__burger--active");
     menu.classList.toggle("header__menu--active");
-});
-
-const year = document.querySelector(".footer__year");
-
-year.innerHTML = new Date().getFullYear();
-
-const submitButton = document.getElementById("submit_form");
-const form = document.getElementById("email_form");
-
-form.addEventListener("submit", (e) => {
-    setTimeout(function () {
-        submitButton.value = "Отправка...";
-        submitButton.disabled = true;
-    }, 1);
 });
