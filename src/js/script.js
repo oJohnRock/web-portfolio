f4b1794958cc05d5ed6b0e4d28f30bb8d7cb481e
m.timurshin <m.timurshin@ittechnogroup.ru>
2022-04-01 16:15:33 +0300

    Обновил шапку и подвал

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@
 
 // Бургер-меню
 const burger = document.querySelector(".header__burger");
-const menu = document.querySelector(".header__menu");
+const menu = document.querySelector(".header__nav");
 burger.addEventListener("click", () => {
     burger.classList.toggle("header__burger--active");
-    menu.classList.toggle("header__menu--active");
+    menu.classList.toggle("header__nav--active");
 });
